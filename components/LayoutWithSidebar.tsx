c2f2121edff0de733ae720a07611b69dc4b3f21c
Ansar786# <insahallah2014@gmail.com>
2025-11-20 07:30:32 +0530

    Re-upload project

--- components/LayoutWithSidebar.tsx
+++ components/LayoutWithSidebar.tsx
@@ -131,7 +131,7 @@ export default function LayoutWithSidebar({ children }: LayoutProps) {
     window.dispatchEvent(new CustomEvent('userLoggedOut'))
     window.dispatchEvent(new Event('storage'))
     
-    router.push('/')
+    router.push('/list-your-business')
   }
 
   // Close dropdown when clicking outside
